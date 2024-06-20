d2b8ea97387ec11b42e3ecfcdb1c0bc8506e7691
massanaRoger <94066091+massanaRoger@users.noreply.github.com>
2024-06-20 22:38:13 +0200

    Draw circle to the screen

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,45 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
 
 #include "utils.h"
-
-using namespace std;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
 }
 
+
+void drawCircle(float centerX, float centerY, float radius, int numSegments, float vertices[][3]) {
+    // Center of the circle
+    vertices[0][0] = centerX;
+    vertices[0][1] = centerY;
+    vertices[0][2] = 0.0f;
+
+    for (int i = 1; i <= numSegments; i++) {
+        float theta = 2.0f * 3.1415926f * float(i - 1) / float(numSegments); // Get the current angle
+
+        float x = radius * glm::cos(theta); // Calculate the x component
+        float y = radius * glm::sin(theta); // Calculate the y component
+
+        vertices[i][0] = x + centerX;
+        vertices[i][1] = y + centerY;
+        vertices[i][2] = 0.0f;
+        std::cout << vertices[i][0] << " " << vertices[i][1] << " "  << vertices[i][2] << std::endl;
+    }
+
+    // Close the circle by adding the first circumference vertex again
+    vertices[numSegments + 1][0] = vertices[1][0];
+    vertices[numSegments + 1][1] = vertices[1][1];
+    vertices[numSegments + 1][2] = vertices[1][2];
+}
+
 int main()
 {
     if (!glfwInit())
     {
-        cout << "Failed to initialize GLFW" << endl;
+        std::cout << "Failed to initialize GLFW" << std::endl;
         return -1;
     }
 
@@ -28,18 +52,18 @@ int main()
     GLFWwindow* window;
     window = glfwCreateWindow(800, 600, "ZMMR", nullptr, nullptr);
     if (window == nullptr) {
-        cout << "Failed to open GLFW window" << endl;
+        std::cout << "Failed to open GLFW window" << std::endl;
         return -1;
     }
     glfwMakeContextCurrent(window);
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
-        cout << "Failed to initialize GLAD" << endl;
+        std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
-    glViewport(0, 0, 800, 600);
+    glViewport(0, 0, 800, 800);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     Shader vertexShader = Shader(getFullPath("shaders/vertex_shader.glsl"), ShaderType::VertexShader);
@@ -67,11 +91,13 @@ int main()
     glDeleteShader(vertexShader.shaderID);
     glDeleteShader(fragmentShader.shaderID);
 
-    float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f,  0.5f, 0.0f
-    };  
+    // Set the circle parameters
+    float centerX = 0.0f;
+    float centerY = 0.0f;
+    float radius = 0.5f;
+    const int numSegments = 100; // The higher the number, the smoother the circle
+    float vertices[numSegments + 2][3];
+    drawCircle(centerX, centerY, radius, numSegments, vertices);
 
     unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
@@ -82,7 +108,7 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
     glEnableVertexAttribArray(0);
 
     glUseProgram(shaderProgram);
@@ -91,7 +117,7 @@ int main()
     {
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, numSegments + 2);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
--- src/shader/Shader.cpp
+++ src/shader/Shader.cpp
@@ -3,7 +3,6 @@
 #include <iostream>
 #include <sstream>
 #include <glad/glad.h>
-#include <GLFW/glfw3.h>
 
 Shader::Shader(const std::string& filePath, const ShaderType shaderType): shaderID(-1) {
     std::string shaderCode;
